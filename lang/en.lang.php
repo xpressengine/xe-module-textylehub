14e0dda663f974384ea15fa6e9a1a544dac1cbdc
supershop <supershop@bebf4043-ad85-6e1f-322f-7830e3d0fd93>
2009-08-21 02:05:24 +0000

    영어 번역 추가
    git-svn-id: http://xe-textyle.googlecode.com/svn/sandbox/textylehub@38 bebf4043-ad85-6e1f-322f-7830e3d0fd93

---
+++ lang/en.lang.php
@@ -0,0 +1,66 @@
+<?php
+    $lang->textylehub = 'Textyle Hub';
+    $lang->about_textylehub = "Textyle Hub shows newest posts and update information of created Textyles.\nIt also allows permitted members to create their own Textyles.";
+
+    $lang->textylehub_id = 'TextyleHub ID';
+    $lang->textylehub_config = 'Setting';
+
+    $lang->textyle_creation_type = 'Textyle Address';
+    $lang->about_textyle_creation_type = 'Users have to determine address type of Textyle.<br/>They can access to http://default URL/SiteID with Site ID. (SiteID is the ID that users have inputted)<br/>Or they can access via the second domain of inputted domain (http://domainID.mydomain.net). (domainID is the ID that users have inputted)';
+
+    $lang->newest_documents_count = 'Newest Posts No.';
+    $lang->about_newest_documents_count = 'You may set the number of newest posts (default 20)';
+
+    $lang->newest_textyles_count = 'Updated Blogs';
+    $lang->about_newest_textyles_count = 'You may set the number of updated blogs (default 10)';
+
+    $lang->textyle_creation_count = 'How Many';
+    $lang->about_textyle_creation_count = 'You may set the number of textyle a permitted member can create. (default 1)';
+
+    $lang->sub_newest_textyles_count = '[Summary] Updated Blogs';
+    $lang->about_sub_newest_textyles_count = 'You may set the number of updated blogs for summary info (default 5)';
+
+    $lang->newest_comments_count = '[Summary] Newest Comments';
+    $lang->about_newest_comments_count = 'You may set the number of newest comments for summary info (default 5)';
+
+    $lang->newest_trackbacks_count = '[Summary] Newest Trackbacks';
+    $lang->about_newest_trackbacks_count = 'You may set the number of newest trackbacks for summary info (default 5)';
+
+    $lang->cmd_create_textylehub = 'New Textyle Hub';
+    $lang->about_create_textylehub = 'There is no Textyle Hub. You can only create 1 Textyle Hub.';
+
+    $lang->success_create_textylehub = 'Textyle Hub has been created';
+
+    $lang->msg_greeting = 'Hello <strong>%s</strong>';
+
+    $lang->created_textyle_count = 'Created Blogs';
+
+    $lang->cmd_create_textyle = 'New Blog';
+
+    $lang->no_textyles = 'There is no blog';
+    $lang->no_posts = 'There is no post';
+    $lang->no_comments = 'There is no comment';
+    $lang->no_trackbacks = 'There is no trackback';
+
+    $lang->textyle_list = 'Blogs List';
+    $lang->updated_textyle = 'Updated Blog';
+    $lang->newest_posts = 'Newest Posts';
+    $lang->newest_comment = 'Newest Comments';
+    $lang->newest_trackback = 'Newest Trackbacks';
+    $lang->posts = 'Posts';
+
+    $lang->msg_disable_to_create_textyle = 'You cannot create a blog';
+
+    $lang->textyle_address = 'Blog Address';
+    $lang->blog_title = 'Blog Title';
+    $lang->blog_description = 'Blog Description';
+    $lang->textyle_agreement = 'Agreement';
+    $lang->cmd_agree = 'I Agree';
+    $lang->about_textyle_agreement = 'You have to agree to agreement prior to creating new blog.';
+    $lang->alert_textyle_address_is_null = 'Please input blog address';
+    $lang->alert_textyle_address_is_wrong = 'Blog address must be 4~12 letters long';
+    $lang->alert_textyle_address_is_exists = 'This address already exists. Please input another one';
+    $lang->alert_textyle_address_format = 'Blog address must start with alphabet, and contain only alphabets or numbers';
+    $lang->alert_textyle_title_is_null = 'Please input title of blog';
+    $lang->alert_disable_to_create = 'You cannot create a blog';
+?>
