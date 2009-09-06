32217144149a89e850414055ef2f2daaf6eef677
c2joy <c2joy@bebf4043-ad85-6e1f-322f-7830e3d0fd93>
2009-09-06 02:55:41 +0000

    중국어 번역 - Textyle Hub
    git-svn-id: http://xe-textyle.googlecode.com/svn/sandbox/textylehub@66 bebf4043-ad85-6e1f-322f-7830e3d0fd93

---
+++ lang/zh-CN.lang.php
@@ -0,0 +1,66 @@
+<?php
+    $lang->textylehub = 'Textyle Hub';
+    $lang->about_textylehub = "Textyle Hub是一个集中显示各个Textyle博客中发布的最新日志及更新信息的中心网页(Hub Page)。\n有权限的用户可以在此直接创建属于自己的博客。";
+
+    $lang->textylehub_id = 'TextyleHub ID';
+    $lang->textylehub_config = 'Textyle Hub设置';
+
+    $lang->textyle_creation_type = 'Textyle生成方式';
+    $lang->about_textyle_creation_type = '在此可指定用户生成Textyle时的访问方式。<br/>站点ID：以http://通行证地址/SiteID方式生成用户个人博客，即用户输入的ID为【站点ID】。<br/>域名：二级域名方式生成用户个人博客，即用户输入的ID为domainID(例 ：http://domainID.mydomain.net)。';
+
+    $lang->newest_documents_count = '最新日志';
+    $lang->about_newest_documents_count = '在此可指定要显示的最新发布日志数(默认为20个)。';
+
+    $lang->newest_textyles_count = '最新更新博客数';
+    $lang->about_newest_textyles_count = '在此可指定最新更新博客数(默认为10个)。';
+
+    $lang->textyle_creation_count = '生成限制';
+    $lang->about_textyle_creation_count = '在此可指定有权限的用户生成博客的个数(默认为1个)。';
+
+    $lang->sub_newest_textyles_count = '最新更新博客数(侧栏)';
+    $lang->about_sub_newest_textyles_count = '在此可指定最新更新博客数(默认为5个)。';
+
+    $lang->newest_comments_count = '最新评论数(侧栏)';
+    $lang->about_newest_comments_count = '在此可指定最新评论数(默认为5个)。';
+
+    $lang->newest_trackbacks_count = '最新引用数(侧栏)';
+    $lang->about_newest_trackbacks_count = '在此可指定最新引用数(默认为5个)。';
+
+    $lang->cmd_create_textylehub = '生成Textyle Hub';
+    $lang->about_create_textylehub = '尚无生成的Textyle Hub。Textyle Hub只能生成一个。';
+
+    $lang->success_create_textylehub = '已成功生成Textyle Hub。';
+
+    $lang->msg_greeting = '您好，<strong>%s</strong>';
+
+    $lang->created_textyle_count = '博客数';
+
+    $lang->cmd_create_textyle = '开通博客';
+
+    $lang->no_textyles = '尚无生成的博客。';
+    $lang->no_posts = '尚无发布的日志。';
+    $lang->no_comments = '尚无发表的评论。';
+    $lang->no_trackbacks = '尚无引用。';
+
+    $lang->textyle_list = '博客列表';
+    $lang->updated_textyle = '最新更新博客';
+    $lang->newest_posts = '最新日志';
+    $lang->newest_comment = '最新评论';
+    $lang->newest_trackback = '最新引用';
+    $lang->posts = '日志';
+
+    $lang->msg_disable_to_create_textyle = '您无权创建博客！';
+
+    $lang->textyle_address = '博客地址';
+    $lang->blog_title = '博客标题';
+    $lang->blog_description = '博客描述';
+    $lang->textyle_agreement = '使用协议';
+    $lang->cmd_agree = '同意使用协议';
+    $lang->about_textyle_agreement = '您必须同意使用协议才能创建博客。';
+    $lang->alert_textyle_address_is_null = '请输入博客地址。';
+    $lang->alert_textyle_address_is_wrong = '博客地址长度限制在4~12个字符。';
+    $lang->alert_textyle_address_is_exists = '重复的博客地址，请重新输入。';
+    $lang->alert_textyle_address_format = '博客地址必须有英文字母开头，并只能使用英文字母及数字。';
+    $lang->alert_textyle_title_is_null = '请输入博客标题。';
+    $lang->alert_disable_to_create = '您无权创建博客！';
+?>
