450883a8993432586e84ead826b3c87e4ade8052
ducduydaovn <ducduydaovn@bebf4043-ad85-6e1f-322f-7830e3d0fd93>
2009-10-20 15:09:59 +0000

    Added Vietnamese Language
    git-svn-id: http://xe-textyle.googlecode.com/svn/sandbox/textylehub@191 bebf4043-ad85-6e1f-322f-7830e3d0fd93

---
+++ lang/vi.lang.php
@@ -0,0 +1,66 @@
+<?php
+    $lang->textylehub = 'Textyle Hub';
+    $lang->about_textylehub = "Textyle Hub hiển thị bài viết mới và những thông tin mới cập nhật của những Textyle đã tạo.\nVà quản lý quyền tạo Textyle cho những thành viên.";
+
+    $lang->textylehub_id = 'TextyleHub ID';
+    $lang->textylehub_config = 'Cài đặt';
+
+    $lang->textyle_creation_type = 'Địa chỉ Textyle';
+    $lang->about_textyle_creation_type = 'Những người sử dụng phải xác định kiểu địa chỉ của Textyle.<br/>Họ có thể tạo dưới dạng http://domain/SiteID với Site ID. (SiteID là ID mà người sử dụng đã nhập vào)<br/>Hay họ có thể tạo dưới dạng một tên miền con (http://domainID.mydomain.net). (domainID là ID mà người sử dụng đã nhập vào)';
+
+    $lang->newest_documents_count = 'Số bài viết mới nhất';
+    $lang->about_newest_documents_count = 'Bạn có thể đặt số bài viết mới nhất sẽ hiển thị (Mặc định là 20)';
+
+    $lang->newest_textyles_count = 'Blog đã cập nhật';
+    $lang->about_newest_textyles_count = 'Bạn có thể thiết đặt số Blog mới cập nhật sẽ hiển thị (Mặc định là 10)';
+
+    $lang->textyle_creation_count = 'Số Blog được tạo';
+    $lang->about_textyle_creation_count = 'Bạn có thể đặt số Blog được phép tạo cho một thành viên. (Mặc định là 1)';
+
+    $lang->sub_newest_textyles_count = 'Số Blog mới tạo';
+    $lang->about_sub_newest_textyles_count = 'Bạn có thể đặt số Blog mới tạo được hiển thị (Mặc định là 5)';
+
+    $lang->newest_comments_count = 'Số bình luận mới nhất';
+    $lang->about_newest_comments_count = 'Bạn có thể đặt số bình luận mới nhất sẽ hiển thị (Mặc định là 5)';
+
+    $lang->newest_trackbacks_count = 'Số liên kết Web mới nhất';
+    $lang->about_newest_trackbacks_count = 'Bạn có thể đặt số liên kết Web mới nhất sẽ hiển thị (Mặc định là 5)';
+
+    $lang->cmd_create_textylehub = 'Tạo Blog';
+    $lang->about_create_textylehub = 'Chưa có Blog nào được tạo. Bạn có thể tạo một Blog cho mình.';
+
+    $lang->success_create_textylehub = 'Blog đã được tạo.';
+
+    $lang->msg_greeting = 'Xin chào <strong>%s</strong>!';
+
+    $lang->created_textyle_count = 'Số Blog đã tạo';
+
+    $lang->cmd_create_textyle = 'Tạo Blog';
+
+    $lang->no_textyles = 'Chưa có Blog nào được tạo/';
+    $lang->no_posts = 'Chưa có bài viết nào.';
+    $lang->no_comments = 'Chưa có bình luận nào.';
+    $lang->no_trackbacks = 'Chưa có liên kết Web nào.';
+
+    $lang->textyle_list = 'Danh sách Blog';
+    $lang->updated_textyle = 'Blog mới cập nhật';
+    $lang->newest_posts = 'Bài viết mới nhất';
+    $lang->newest_comment = 'Bình luận mới nhất';
+    $lang->newest_trackback = 'Liên kết Web mới nhất';
+    $lang->posts = 'Gửi bài mới';
+
+    $lang->msg_disable_to_create_textyle = 'Bạn không thể tạo Blog.';
+
+    $lang->textyle_address = 'Địa chỉ Blog';
+    $lang->blog_title = 'Tiêu đề Blog';
+    $lang->blog_description = 'Mô tả cho Blog';
+    $lang->textyle_agreement = 'Điều khoản sử dụng';
+    $lang->cmd_agree = 'Tôi đồng ý với điều khoản sử dụng này.';
+    $lang->about_textyle_agreement = 'Bạn phải đồng ý với những điều khoản sử dụng của chúng tôi mới có thể tạo được Blog.';
+    $lang->alert_textyle_address_is_null = 'Xin hãy nhập địa chỉ cho Blog';
+    $lang->alert_textyle_address_is_wrong = 'Địa chỉ của Blog phải từ 4 - 12 kí tự.';
+    $lang->alert_textyle_address_is_exists = 'Địa chỉ này đã được tạo. Xin hãy thử lại với địa chỉ khác.';
+    $lang->alert_textyle_address_format = 'Địa chỉ Blog chỉ cho phép sử dụng chữ cái hoặc chữ cái và số.';
+    $lang->alert_textyle_title_is_null = 'Xin hãy nhập tiêu đề cho Blog.';
+    $lang->alert_disable_to_create = 'Bạn không thể tạo được Blog.';
+?>
