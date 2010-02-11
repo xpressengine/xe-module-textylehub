7690dc7a10b353a90a0d12a61623e9f7b06c3154
royallin <royallin@bebf4043-ad85-6e1f-322f-7830e3d0fd93>
2010-02-11 01:33:31 +0000

    Update TW language
    git-svn-id: http://xe-textyle.googlecode.com/svn/sandbox/textylehub@401 bebf4043-ad85-6e1f-322f-7830e3d0fd93

---
+++ lang/zh-TW.lang.php
@@ -0,0 +1,72 @@
+<?php
+    /**
+     * @file   zh-TW.lang.php
+     * @author ? 翻譯：royallin
+     * @brief  Textyle Hub正體中文語言
+     **/
+
+    $lang->textylehub = 'Textyle Hub';
+    $lang->about_textylehub = "Textyle Hub는 분양된 Textyle의 최신글을 모아 보여주고 또 생성된 Textyle의 업데이트 정보를 제공합니다.\n또한 권한이 있는 회원이 직접 Textyle을 개설할 수 있도록 합니다";
+
+    $lang->textylehub_id = 'TextyleHub ID';
+    $lang->textylehub_config = 'Textyle Hub 設定';
+
+    $lang->textyle_creation_type = 'Textyle 建立方式';
+    $lang->about_textyle_creation_type = '可決定使用者建立Textyle之後的訪問方式。<br/>Site ID是用 http://網址/SiteID 所建立的。(使用者輸入的ID就是Site ID)<br/>Domain 是用次級域名(http://domainID.mydomain.net) 的方式建立。 (就是使用者輸入的ID是domainID)';
+
+    $lang->newest_documents_count = '最新日誌';
+    $lang->about_newest_documents_count = '최근 올라온글의 목록 수를 정할 수 있습니다 (預設 20個)';
+
+    $lang->newest_textyles_count = '업데이트 된 블로그 수';
+    $lang->about_newest_textyles_count = '업데이트된 블로그 수를 정할 수 있습니다 (預設 10個)';
+
+    $lang->textyle_creation_count = '생성 개수';
+    $lang->about_textyle_creation_count = '권한이 있는 회원 1인당 생성할 수 있는 개수를 정할 수 있습니다. (預設 1個)';
+
+    $lang->sub_newest_textyles_count = '[요약] 업데이트 된 블로그 수';
+    $lang->about_sub_newest_textyles_count = '요약정보의 업데이트된 블로그 수를 정할 수 있습니다 (預設 5個)';
+
+    $lang->newest_comments_count = '[요약] 최근 댓글 수';
+    $lang->about_newest_comments_count = '요약정보의 최근 댓글 수를 정할 수 있습니다 (기본 5)';
+
+    $lang->newest_trackbacks_count = '[요약] 최근 엮인글 수';
+    $lang->about_newest_trackbacks_count = '요약정보의 최근 엮인글 수를 정할 수 있습니다 (기본 5)';
+
+    $lang->cmd_create_textylehub = 'Textyle Hub 생성';
+    $lang->about_create_textylehub = 'Textyle Hub가 생성되지 않았습니다. Textyle Hub는 하나만 생성 가능합니다.';
+
+    $lang->success_create_textylehub = 'Textyle Hub가 생성되었습니다';
+
+    $lang->msg_greeting = '<strong>%s</strong>님 안녕하세요';
+
+    $lang->created_textyle_count = '생성된 블로그';
+
+    $lang->cmd_create_textyle = '새 블로그 만들기';
+
+    $lang->no_textyles = '블로그가 없습니다';
+    $lang->no_posts = '등록된 글이 없습니다';
+    $lang->no_comments = '댓글이 없습니다';
+    $lang->no_trackbacks = '트랙백이 없습니다';
+
+    $lang->textyle_list = '블로그 목록';
+    $lang->updated_textyle = '업데이트 된 블로그';
+    $lang->newest_posts = '최근 올라온 글';
+    $lang->newest_comment = '최근 댓글';
+    $lang->newest_trackback = '최근 트랙백';
+    $lang->posts = '게시글';
+
+    $lang->msg_disable_to_create_textyle = '블로그를 개설할 수 없습니다';
+
+    $lang->textyle_address = '블로그 주소';
+    $lang->blog_title = '블로그 제목';
+    $lang->blog_description = '한줄 소개';
+    $lang->textyle_agreement = '이용 약관';
+    $lang->cmd_agree = '동의합니다';
+    $lang->about_textyle_agreement = '이용약관에 동의해야만 블로그를 만들 수 있습니다.';
+    $lang->alert_textyle_address_is_null = '블로그 주소를 입력해주셔야 합니다';
+    $lang->alert_textyle_address_is_wrong = '블로그 주소는 4~12 사이로 입력하셔야 합니다';
+    $lang->alert_textyle_address_is_exists = '이미 존재하는 블로그주소입니다. 다른 블로그 주소를 입력해주세요';
+    $lang->alert_textyle_address_format = '블로그 주소는 영문으로 시작하고 영문,숫자로만 이루어져야 합니다';
+    $lang->alert_textyle_title_is_null = '블로그 제목을 입력해주셔야 합니다';
+    $lang->alert_disable_to_create = '블로그를 생성할 수 없습니다';
+?>
