d0649dfe3de9055d5a49f9bdda1a9c6945f885a7
ngleader <ngleader@gmail.com>
2011-02-23 09:46:47 +0000

    r505 add Turkey langauge
    git-svn-id: http://xe-textyle.googlecode.com/svn/sandbox/textylehub@508 bebf4043-ad85-6e1f-322f-7830e3d0fd93

---
+++ lang/tr.lang.php
@@ -0,0 +1,66 @@
+<?php
+    $lang->textylehub = 'Textyle Hub';
+    $lang->about_textylehub = "Textyle Hub, oluşturulmuş Textyle/'larin güncel bilgilerini ve son mesajları gösterir.\n Ayrıca yetkili üyelere kendi Textyle/'larını oluşturma izni verir.";
+
+    $lang->textylehub_id = 'TextyleHub Kimlik bilgisi';
+    $lang->textylehub_config = 'Ayarlar';
+
+    $lang->textyle_creation_type = 'Textyle Adresi';
+    $lang->about_textyle_creation_type = 'Textyle ın adres biçimine kullanıcılar karar vermek zorundadır.<br/>Site kimlik bilgisi ile http://default URL/SiteID adresine girebilirler. (Site kimlik bilgisi kullanıcıların girdiği kimlik bilgileridir.)<br/>Yada ikinci alan adı yoluyla siteye giriş yapabilirler. (http://domainID.mydomain.net). (Alan adı kimlik bilgileri kullanıcıların girdiği bilgilerdir.)';
+
+    $lang->newest_documents_count = 'Son Mesaj No.';
+    $lang->about_newest_documents_count = 'Son mesajların sayısını siz belirleyebilirsiniz. (varsayılan 20)';
+
+    $lang->newest_textyles_count = 'Güncellenmiş Bloglar';
+    $lang->about_newest_textyles_count = 'Güncellenmiş blogların sayısını siz belirleyebilirsiniz. (varsayılan 10)';
+
+    $lang->textyle_creation_count = 'Kaç Tane';
+    $lang->about_textyle_creation_count = 'Yetkili bir üyenin oluşturabileceği textyle sayısını belirleyebilirsiniz. (varsayılan 1)';
+
+    $lang->sub_newest_textyles_count = '[Özet] Güncellenmiş Bloglar';
+    $lang->about_sub_newest_textyles_count = 'Özet bilgiler için güncellenmiş blogların sayısını belirleyebilirsiniz.(varsayılan 5)';
+
+    $lang->newest_comments_count = '[Özet] Son Yorumlar';
+    $lang->about_newest_comments_count = 'Özet bilgiler için son yorumların sayısını belirleyebilirsiniz. (varsayılan 5)';
+
+    $lang->newest_trackbacks_count = '[Özet] Son Geri İzlemeler';
+    $lang->about_newest_trackbacks_count = 'Özet bilgiler için son geri izleme sayısını belirleyebilirsiniz. (varsayılan 5)';
+
+    $lang->cmd_create_textylehub = 'Yeni Textyle Hub';
+    $lang->about_create_textylehub = 'Textyle Hub bulunmamaktadır. Sadece 1 tane Textyle Hub oluşturabilirsiniz.';
+
+    $lang->success_create_textylehub = 'Textyle Hub oluşturuldu';
+
+    $lang->msg_greeting = 'Merhaba <strong>%s</strong>';
+
+    $lang->created_textyle_count = 'Oluşturulmuş Bloglar';
+
+    $lang->cmd_create_textyle = 'Yeni Blog';
+
+    $lang->no_textyles = 'Blog bulunmamaktadır';
+    $lang->no_posts = 'Mesaj bulunmamaktadır';
+    $lang->no_comments = 'Yorum bulunmamaktadır';
+    $lang->no_trackbacks = 'Geri izleme bulunmamaktadır';
+
+    $lang->textyle_list = 'Blog Listesi';
+    $lang->updated_textyle = 'Güncellenmiş Blog';
+    $lang->newest_posts = 'Son Mesajlar';
+    $lang->newest_comment = 'Son Yorumlar';
+    $lang->newest_trackback = 'Son Geri İzlemeler';
+    $lang->posts = 'Mesajlar';
+
+    $lang->msg_disable_to_create_textyle = 'Blog oluşturma yetkiniz yok';
+
+    $lang->textyle_address = 'Blog Adresi';
+    $lang->blog_title = 'Blog Başlığı';
+    $lang->blog_description = 'Blog Açıklaması';
+    $lang->textyle_agreement = 'Anlaşma';
+    $lang->cmd_agree = 'Kabul ediyorum';
+    $lang->about_textyle_agreement = 'Yeni blog oluşturmadan önce anlaşmayı onaylamanız gerekmektedir.';
+    $lang->alert_textyle_address_is_null = 'Lütfen blog adresini girin';
+    $lang->alert_textyle_address_is_wrong = 'Blog adresi 4~12 karakter uzunluğu arasında olmalıdır.';
+    $lang->alert_textyle_address_is_exists = 'Bu adres zaten kullanımda. Lütfen yeni bir adres girin';
+    $lang->alert_textyle_address_format = 'Blog adresi bir harfle başlamalı ve sadece harf veya sayı içermelidir.';
+    $lang->alert_textyle_title_is_null = 'Lütfen blog başlığını girin';
+    $lang->alert_disable_to_create = 'Blog oluşturamazsınız';
+?>
